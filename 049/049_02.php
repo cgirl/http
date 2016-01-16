a456c70ca553bab60101296b5f32794eb697ac78
cgirl <1579090779@qq.com>
2016-01-16 17:18:34 +0800

    comet反向ajax

---
+++ 049/049_02.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * comet反向ajax
+ */
+header("content-type:text/html;charset=utf-8");
+
+set_time_limit(0); //设置超时时间为0，即永不超时
+ob_start(); //开启缓冲区
+
+$pad = str_repeat(' ', 4000);
+echo $pad,'<br />';
+ob_flush(); //释放缓冲区内容
+flush(); //把缓冲区释放的内容，立即送给浏览器，而不要等脚本结束再一起送
+
+$conn = mysql_connect('localhost', 'root', '123456');
+mysql_query('use test');
+
+$i = 1;
+while ($i){
+	echo $pad,'<br />';
+	$sql = 'select * from msg where flag=0';
+	$res = mysql_query($sql);
+	$row = mysql_fetch_assoc($res);
+	
+	if (!empty($row)){
+		echo $pad, '<br />';
+		echo $row['content'], '<br />';
+		mysql_query('update msg set flag=1');
+	}
+	ob_flush(); //释放缓冲区内容
+	flush(); //把缓冲区释放的内容，立即送给浏览器，而不要等脚本结束再一起送
+}
+
+/*
+ * 思考：如果while循环中，不是1,2,3....
+ * 而是数据库中的内容呢？
+ * 或者是2人的聊天记录内容呢？
+ * 这样就成达到即时通信
+ * 
+ * 服务器端--不间断--推送信息-->到客户端
+ */
+?>
